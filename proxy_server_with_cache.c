28bb7d5203e482123336c5306143d9b482dfcacf
RAJA PANDEY <92283721+Rpandey010@users.noreply.github.com>
2024-06-20 22:04:53 +0530

    till thread function

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -43,7 +43,7 @@ int cache_size;
 
 int main(int argc, char* argv[]){
     int client_socketId, client_len;
-    struct sockaddr serve_addr, client_addr;
+    struct sockaddr_in server_addr, client_addr;
     sem_init(&semaphore, 0, MAX_CLIENTS);
     pthread_mutex_init(&lock, NULL);
     if(argv == 2){
@@ -55,4 +55,53 @@ int main(int argc, char* argv[]){
 
     printf("Starting Proxy server at port %d\n", port_number);
     proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (proxy_socketId < 0){
+        perror("Failed to create a socket\n");
+        exit(1);
+    }
+    int reuse = 1;
+    if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(reuse)) < 0){
+        perror("Failed to set socket options\n");
+    }
+
+    bzero((char*)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr) < 0)){
+        perror("Failed to bind the socket Port is not available\n");
+        exit(1);
+    }
+    printf("Binding on port %d\n", port_number);
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+    if(listen_status < 0){
+        perror("Failed to listen on the socket\n");
+        exit(1);
+    }
+    int i = 0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1){
+        bzero((char*)&client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_socketId = accept(proxy_socketId, (struct sockaddr *)&client_addr, (socklen_t*)&client_len);
+        if(client_socketId < 0){
+            printf("Not able to connect");
+            exit(1);
+        }
+        else{
+            Connected_socketId[i] = client_socketId;
+        }
+        struct sockaddr_in * client_pt = (struct sockaddr_in *)&client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &ip_addr, str, INET_ADDRSTRLEN);
+        printf("Client is connected with port number %d and IP address %s\n", ntohs(client_addr.sin_port), str);
+
+        pthread_create(&tid[i], NULL, thread_fn, (void *)&Connected_socketId[i]);
+        i++;
+    }
+    close(proxy_socketId);
+    return 0;
 }
